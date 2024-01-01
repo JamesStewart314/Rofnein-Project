2f2e94b775d2fcbb703a45a0528bf54fc88fabee
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2023-12-31 21:19:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from pygame import Surface
+
 import Assets.Player_Images  # To avoid circular import
 
 from Assets import Background_Images
@@ -9,17 +11,17 @@ from MyFunctions import MyFunctions_2
 
 pygame.init()  # Initializing Pygame.
 
-Screen = Worlds.Screen  # Creating the Game Window.
-
-is_fullscreen = False
+is_fullscreen: bool = False
 
 pygame.display.set_caption("Rofnein")  # Setting the Title
 pygame.display.set_icon(Background_Images.Board_Leaf_Icon)  # Changing the Window Icon.
 
 # Creating the Game Window :
-Screen = pygame.display.set_mode((Game_Constants.Window_width, Game_Constants.Window_height)) if not is_fullscreen \
-        else pygame.display.set_mode((Game_Constants.Window_width, Game_Constants.Window_height), pygame.FULLSCREEN)
+Screen: Surface = pygame.display.set_mode(
+    (Game_Constants.Window_width, Game_Constants.Window_height)) if not is_fullscreen \
+    else pygame.display.set_mode((Game_Constants.Window_width, Game_Constants.Window_height), pygame.FULLSCREEN)
 
-
-MyFunctions_2.menu(Screen)
-
+try:
+    MyFunctions_2.menu(Screen)
+except pygame.error:
+    pass
